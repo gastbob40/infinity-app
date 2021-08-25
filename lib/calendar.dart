24a108e149f33ed00ce9f05861f5d18e06008244
gastbob40 <gastbob40@gmail.com>
2021-08-25 16:28:47 +0200

    feat (app): add all widgets (WIP)

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 const Color blockColor = Color(0xFF252525);
+const Color separator = Color(0x1FFFFFFF);
 
 class Calendar extends StatelessWidget {
   @override
@@ -31,7 +33,9 @@ class Calendar extends StatelessWidget {
                   Text(
                     'Septembre',
                     style: GoogleFonts.rubik(
-                        fontSize: 30, fontWeight: FontWeight.w100, color: Colors.white60),
+                        fontSize: 30,
+                        fontWeight: FontWeight.w100,
+                        color: Colors.white60),
                   )
                 ],
               ),
@@ -97,14 +101,84 @@ class Calendar extends StatelessWidget {
                         Text(
                           e[1].toString(),
                           style: GoogleFonts.rubik(
-                            fontSize: 11,
-                            color: Colors.white60
-                          ),
+                              fontSize: 11, color: Colors.white60),
                         )
                       ]))
                   .toList(),
             ),
           ),
+          SizedBox(
+            height: 30,
+          ),
+          SingleChildScrollView(
+            child: Column(children: [
+              Container(
+                  decoration: BoxDecoration(
+                    color: blockColor,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black54,
+                        blurRadius: 1,
+                        offset: Offset(0, 1), // Shadow position
+                      )
+                    ],
+                  ),
+                  height: 100,
+                  padding: EdgeInsets.all(16),
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                              right: BorderSide(width: 1, color: separator)),
+                        ),
+                        padding: EdgeInsets.fromLTRB(0, 0, 16, 0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '9h00',
+                              style: GoogleFonts.rubik(
+                                  fontWeight: FontWeight.w100),
+                            ),
+                            Text(
+                              '10h00',
+                              style: GoogleFonts.rubik(
+                                  fontWeight: FontWeight.w100),
+                            )
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                          child: Container(
+                            padding: EdgeInsets.fromLTRB(16, 0, 0, 0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Séminaire Algo', style: GoogleFonts.rubik()),
+                            SizedBox(height: 10,),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(children: [
+                                  Icon(Icons.place_outlined, size: 11, color: Colors.white60,),
+                                  Text('VA 106', style: GoogleFonts.rubik(fontSize: 11, color: Colors.white60),)
+                                ],),
+                                Row(children: [
+                                  Icon(Icons.person_outlined, size: 11, color: Colors.white60,),
+                                  Text('Nom Prenom', style: GoogleFonts.rubik(fontSize: 11, color: Colors.white60),)
+                                ],)
+                              ],
+                            )
+                          ],
+                        ),
+                      ))
+                    ],
+                  ))
+            ]),
+          )
         ],
       ),
     ));
