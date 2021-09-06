c992d981fc74f62066160aa8f4a184e5e8aa8426
gastbob40 <gastbob40@gmail.com>
2021-09-07 00:06:49 +0200

    feat (calendar): add coming soon for news

--- lib/news.dart
+++ lib/news.dart
@@ -5,40 +5,8 @@ class News extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return SafeArea(
-      child: Column(
-        children: [
-          SizedBox(height: 50,),
-          Flexible(
-            child: PageView(
-              children: <Widget>[
-                Container(
-                  color: Colors.orange,
-                  child: Center(
-                      child: Text(
-                    'Page 1',
-                    style: TextStyle(color: Colors.white),
-                  )),
-                ),
-                Container(
-                  color: Colors.blue,
-                  child: Center(
-                      child: Text(
-                    'Page 2',
-                    style: TextStyle(color: Colors.white),
-                  )),
-                ),
-                Container(
-                  color: Colors.green,
-                  child: Center(
-                      child: Text(
-                    'Page 3',
-                    style: TextStyle(color: Colors.white),
-                  )),
-                ),
-              ],
-            ),
-          )
-        ],
+      child: Center(
+        child: Text("Coming soon"),
       ),
     );
   }
